feea0697ba659c01579d2072e527f7e80a55ed5f
vaniamc <vania.maya07@gmail.com>
2017-07-19 09:36:39 +0700

    benerin carousel

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,7 +9,7 @@
 					<ol class="carousel-indicators">
 						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 						<?php for($i = 1; $i < $info->count(); $i++):
-						echo "<li data-target="."#myCarousel"." data-slide-to=".$i." class="."active"."></li>";
+						echo "<li data-target="."#myCarousel"." data-slide-to=".$i."></li>";
 						endfor;
 						?>
 					</ol>
